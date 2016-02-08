a09cd0f390d84bfb52e3cc303aabfe8dee108b44
ramesh081 <rameshkumar.iimt@gmail.com>
2016-02-08 19:32:12 +0530

    first commit

---
+++ public/core.js
@@ -0,0 +1,53 @@
+// public/core.js
+var bogoTodo = angular.module('bogoTodo', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    // when landing on the page, get all todos and show them
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function(id) {
+
+        var textbox = document.getElementById("textbox");
+    if( textbox.value.length >= 5){
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+            document.getElementById("textbox").style.color="";
+    }
+    else{
+
+        document.getElementById("textbox").style.color = "red";
+        alert('value length must be greater than 5');
+    }
+        
+    };
+
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}
